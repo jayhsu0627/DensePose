f11f3aac75f3ee44f6cb18172dba28153101eb97
Shengjie Xu <sjxu@umd.edu>
2024-05-17 09:55:29 -0400

    edit sh

--- DensePoseData/get_DensePose_COCO.sh
+++ DensePoseData/get_DensePose_COCO.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir DensePose_COCO
 cd DensePose_COCO
-wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_train.json
-wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_valminusminival.json
-wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_minival.json
-wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_test.json
+wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_train.json --no-check-certificate
+wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_valminusminival.json --no-check-certificate
+wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_minival.json --no-check-certificate
+wget https://dl.fbaipublicfiles.com/densepose/densepose_coco_2014_test.json --no-check-certificate
--- DensePoseData/get_eval_data.sh
+++ DensePoseData/get_eval_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir eval_data
 cd eval_data
-wget https://dl.fbaipublicfiles.com/densepose/densepose_eval_data.tar.gz
+wget https://dl.fbaipublicfiles.com/densepose/densepose_eval_data.tar.gz --no-check-certificate
 tar xvf densepose_eval_data.tar.gz
 rm densepose_eval_data.tar.gz
